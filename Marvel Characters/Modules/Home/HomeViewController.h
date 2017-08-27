329f9682c1329c2fe4e3712ab21df2d3a2de26cd
Ruslan Kuchukbaev <goidoyuuji@gmail.com>
2017-08-27 13:20:08 +0300

    initialize project structure

---
+++ Marvel Characters/Modules/Home/HomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.h
+//  Marvel Characters
+//
+//  Created by Ruslan Kuchukbaev on 27/08/2017.
+//  Copyright © 2017 Zimad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeViewController : UIViewController
+
+@end
